92e78f50fad29ac40987c8b9a8bb5512984d5115
Josh Morrison <joshuarmorrison@gmail.com>
2021-08-24 14:46:17 -0600

    finished loop

--- app/loops.js
+++ app/loops.js
@@ -6,8 +6,16 @@
 // 1*2*3*4 = 24
 
 function factorials(num) {
-
+    if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 }
+
+
+
 
 // ------------------------------------------
 
@@ -16,19 +24,34 @@ function factorials(num) {
 // input: 2, 6
 // output: 20
 
-function rangeTotal(n1, n2) {
+// n1 = 2
+// n2 = 6
+// i = 6
+// sum = 14
 
-}
+function rangeTotal(n1, n2) {
+  let sum = 0
+   for(let i=n1; i <= n2; i++){
+    sum = sum + i
+   }
+   return sum
+}  
 
 // ------------------------------------------
 
 
 // 3. Write a function that will take in a starting number, ending number, and a divisor. Iterate
-// over the range of the numbers and rtuen how many numbers can be divided by the divisor.
+// over the range of the numbers and return how many numbers can be divided by the divisor.
 // Example:
 // input: 15, 20, 5
 // output: 2
 
 function rangeDivisor(start, end, divisor) {
-
+let x = 0
+for(let i = start ; i <= end ; i++){
+    if(i % divisor === 0){
+        x++  
+    }
+}
+return x
 }
